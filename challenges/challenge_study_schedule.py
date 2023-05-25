193b64342589c243e530c427656614bed52429fe
Lucas G <lucasg.devmail93@gmail.com>
2023-05-25 16:04:45 -0300

    feat: function study_schedule implemented!

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if 0 > target_time or target_time > 23:
+        return None
+
+    result = 0
+    for i in permanence_period:
+        if 0 > i[0] or i[0] > 23 or 0 > i[1] or i[1] > 23:
+            print('vallidate intervall')
+            return None
+        elif i[0] <= target_time and target_time <= i[1]:
+            print(f"entrada: {i[0]}, saída: {i[1]}, target_time {target_time}")
+            result += 1
+    return result
