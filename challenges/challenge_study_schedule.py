75ba6bd53465a30297d7d739ecc4171c1cf0077a
Lucas G <lucasg.devmail93@gmail.com>
2023-05-25 17:26:05 -0300

    fix: function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -6,13 +6,8 @@ def study_schedule(permanence_period, target_time):
 
     result = 0
     for i in permanence_period:
-        if 0 > i[0] or i[0] > 23 or 0 > i[1] or i[1] > 23:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
             return None
         elif i[0] <= target_time and target_time <= i[1]:
             result += 1
     return result
-
-
-lista = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-hour = 2
-print(study_schedule(lista, "x"))
