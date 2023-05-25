f9353cd6eba8d41a5943a61263dceda640bccef7
Lucas G <lucasg.devmail93@gmail.com>
2023-05-25 18:18:58 -0300

    feat: implemented function is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    w, l, h = [word, low_index, high_index]
+
+    if word == "":
+        return False
+    if w[l] == w[h] and h - l > 0:
+        print(f"l: {w[l]} - r: {w[h]}")
+        return is_palindrome_recursive(word, l + 1, h - 1)
+    elif w[l] != w[h]:
+        return False
+    else:
+        return True
